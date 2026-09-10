3467639243889763707d10fd8f752f7cd23fbd6c
Wilber Banos <wilber930328@gmail.com>
2026-09-10 07:15:21 -0400

    adding google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -239,6 +239,27 @@ export default function RootLayout({
   return (
     <html lang="en" data-scroll-behavior="smooth">
       <head>
+        
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({
+                  'gtm.start': new Date().getTime(),
+                  event: 'gtm.js'
+                });
+                var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),
+                    dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NTDHKLLQ');
+            `,
+          }}
+        />
+
         <link
           rel="preconnect"
           href="https://cdn.sanity.io"
@@ -288,6 +309,17 @@ export default function RootLayout({
       </head>
 
       <body>
+
+        <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-NTDHKLLQ"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+          title="Google Tag Manager"
+        />
+        </noscript>
+
         <ErrorBoundary>
           <Providers>
             <LoadingProvider>
